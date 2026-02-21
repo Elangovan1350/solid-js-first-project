11b666a36ff133d98ca32a4b1f9382b3bbc34efb
Elangovan1350 <elangovan2019miss@gmail.com>
2026-02-21 21:15:02 +0530

    change1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import type { Component } from "solid-js";
-import { createSignal, createEffect, onMount } from "solid-js";
+import { createSignal, createEffect, onMount, For } from "solid-js";
+import axios from "axios";
+
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  [key: string]: any;
+}
 
 const App: Component = () => {
   const [count, setCount] = createSignal(0);
   const [isDark, setIsDark] = createSignal(false);
+  const [products, setProducts] = createSignal<Product[]>([]);
+  const [loading, setLoading] = createSignal(true);
 
   onMount(() => {
     console.log("Component mounted");
+    axios.get("https://dummyjson.com/products").then((res) => {
+      console.log(res.data);
+      setProducts(res.data.products);
+      setLoading(false);
+    });
+  });
+
+  createEffect(() => {
+    console.log("Products: ", products());
   });
 
   createEffect(() => {
@@ -20,7 +40,7 @@ const App: Component = () => {
   return (
     <>
       <div
-        class={` h-screen w-screen flex flex-col justify-center items-center ${isDark() ? "bg-black text-white" : "bg-white text-black"}`}
+        class={` flex flex-col justify-center items-center ${isDark() ? "bg-black text-white" : "bg-white text-black"}`}
       >
         <button onClick={() => setIsDark((prev) => !prev)}>Toggle</button>
         <p class="text-4xl text-green-700 text-center py-20">
@@ -29,6 +49,19 @@ const App: Component = () => {
         <button onClick={() => setCount((prev) => prev + 1)}>
           Count: {count()}
         </button>
+        {loading() ? (
+          <p>Loading...</p>
+        ) : (
+          <ul>
+            <For each={products()}>
+              {(product) => (
+                <li class=" py-5 text-2xl text-red-500 font-bold ">
+                  {product.title}
+                </li>
+              )}
+            </For>
+          </ul>
+        )}
       </div>
     </>
   );
